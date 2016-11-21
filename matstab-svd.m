6f8c4194d25eded81baf5c87a956bb88b0f543bb
Ganesh Rakate <ganeshrs@users.noreply.github.com>
2016-11-20 23:25:00 -0600

    Update matstab-svd.m

--- matstab-svd.m
+++ matstab-svd.m
@@ -1,5 +1,6 @@
 
 % Stability of a matrix with SVD method
+% Ganesh
 
 A = randn(10,3); 
 A=A./repmat(sqrt(sum(A.^2)),10,1);
@@ -7,6 +8,9 @@ A=A./repmat(sqrt(sum(A.^2)),10,1);
 B = A + 0.1*randn(10,3); 
 B = B./repmat(sqrt(sum(B.^2)),10,1);
 
-[Ud,Sd,Vd]=svd(A-B)
-[Ua,Sa,Va]=svd(A)
+[Ud,Sd,Vd]=svd(A-B);
+[Ua,Sa,Va]=svd(A);
+
+rd = sum(diag(Sd));
+ra = sum(diag(Sa));
 s = rd/ra;
